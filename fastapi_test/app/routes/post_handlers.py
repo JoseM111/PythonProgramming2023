09e3ad3a5d72ca857e09af9ad39a96c57b2db07c
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-04-26 09:41:21 -0400

    🫥  devepic1 ≡ -->Displaying custom type ResultType..

--- fastapi_test/app/routes/post_handlers.py
+++ fastapi_test/app/routes/post_handlers.py
@@ -1,5 +1,7 @@
 # file: routes/post_handlers.py
 # =============================================
+import uuid
+from uuid import UUID
 
 from fastapi import APIRouter, HTTPException
 from app.models.models import Post
@@ -8,8 +10,16 @@ from app.models.models import Post
 router = APIRouter()
 
 MY_POST = [
-  { "title": "title post 1", "content": "First post", "id": 1 },
-  { "title": "title post 2", "content": "Second post", "id": 2 },
+  dict(
+    id="64867f5b-ef55-4128-808f-77a55f6154b0",
+    title="title post 1",
+    content="First post"
+  ),
+  dict(
+    id="f89e5b43-a46d-43d6-b49c-897653b4a4da",
+    title="title post 2",
+    content="Second post"
+  ),
 ]
 # =============================================
 
@@ -19,15 +29,16 @@ async def health_check() -> dict[str, str]:
 
 @router.post("/posts")
 def create_post(post: Post):
-  # increments the id when a new post is created
-  post_id = len(MY_POST) + 1
+  # Generate a new UUID for the post ID
+  post_id: UUID = uuid.uuid4()
+  print(f"Generated UUID: {post_id}, type: {type(post_id)}")
   post.id = post_id
 
   post_to_dict = post.dict(
     exclude_none=True,
     exclude={
-      'published': post.published is None,
-      'rating': post.rating is None
+      "published": post.published is None,
+      "rating": post.rating is None
     }
   )
 
@@ -38,22 +49,21 @@ def create_post(post: Post):
 async def get_all_post():
   return { "data": MY_POST }
 
-@router.get("/posts/{id}")
-async def get_post(post_id: int):
-  return { "post_details": f"Here is post {post_id}" }
+@router.get("/posts/{post_id}")
+async def get_post_by_id(post_id: UUID):
+  print(f"Searching for post_id: {post_id}")
 
+  for post in MY_POST:
+    print(f"Comparing: {post['id']} with {str(post_id)}")
 
-# @router.get("/book-by-index/{index}")
-# async def book_by_index(index: int) -> dict[str, str]:
-#   if index < 0 or index >= len(BOOK_DB):
-#     # FAIL
-#     raise HTTPException(
-#       status_code=404,
-#       detail=f"Index {index} is out of range {len(BOOK_DB)}"
-#     )
-#   else:
-#     return { "book": BOOK_DB[index] }
-# =============================================
+    if post["id"] == str(post_id):
+      print("Match found, returning the post.")
+      return { "post_details": post }
+
+  raise HTTPException(
+    status_code=404,
+    detail=f"Post with ID {post_id} not found"
+  )
 
 
 
