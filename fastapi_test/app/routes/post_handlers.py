aca023de49c18640f28ea377791c5ddf561a5b89
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-04-26 21:53:58 -0400

    🚀 dev/epic2 ≡ ->still fixing a bunch of uncommited branch confusion..

--- fastapi_test/app/routes/post_handlers.py
+++ fastapi_test/app/routes/post_handlers.py
@@ -5,6 +5,8 @@ import uuid
 from uuid import UUID
 
 from fastapi import APIRouter, HTTPException
+from starlette import status
+
 from app.models.models import Post
 # =============================================
 
@@ -24,11 +26,11 @@ MY_POST = [
 ]
 # =============================================
 
-@router.get("/")
+@router.get(path="/")
 async def health_check() -> dict[str, str]:
   return { "Message": "Welcome to my health check!.." }
 
-@router.post("/posts")
+@router.post(path="/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
   # Generate a new UUID for the post ID
   post_id: UUID = uuid.uuid4()
@@ -49,7 +51,7 @@ def create_post(post: Post):
   MY_POST.append(post_to_dict)
   return { "data": post_to_dict }
 
-@router.get("/posts")
+@router.get(path="/posts", status_code=status.HTTP_200_OK)
 async def get_all_post():
   return { "data": MY_POST }
 
@@ -67,7 +69,17 @@ async def get_post_by_id(post_id: UUID):
     detail=f"Post with ID {post_id} not found"
   )
 
+@router.delete(path="/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(post_id: UUID):
+  for index, post in enumerate(MY_POST):
+    if str(post["id"]) == str(post_id):
+      del MY_POST[index]
+      return
 
+  raise HTTPException(
+    status_code=status.HTTP_404_NOT_FOUND,
+    detail=f"Post with ID {post_id} not found"
+  )
 
 
 
